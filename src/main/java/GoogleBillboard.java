ae12718bdc3403738aa28ba7fd5b581d6dce2e5b
Ezra-sqr <143028029+Ezra-sqr@users.noreply.github.com>
2024-01-16 12:35:22 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,7 +1,7 @@
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-public void setup()  
-{   
-  boolean found=false;
+public class GoogleBillboard {
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public static void main(final String[] args) {
+		boolean found=false;
     for(int i=2; i<e.length()-10; i++){
      String dotcom=e.substring(i,i+10);
      double num =Double.parseDouble(dotcom);
@@ -10,13 +10,11 @@ public void setup()
      found=true;
    }
     }
-}  
-public void draw()  
-{   
-  //not needed for this assignment
-}  
-public boolean isPrime(double dNum)  
-{   
+	}
+
+
+	//Finish this function
+	public boolean isPrime(double dNum){
    if (dNum<2)
     return false;
   for (int i=2; i<=Math.sqrt(dNum);i++){
@@ -24,4 +22,7 @@ public boolean isPrime(double dNum)
     return false;
   }
   return true;
-} 
+    
+	}
+
+}
